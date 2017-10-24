3e0b4ae570fd56f3d9e8d53bddbd22b1c1a6d098
pdong <pdong@Ctrip.com>
2017-10-24 17:58:19 +0800

    Add ShooterSub

--- ShooterSub/DownloadSub_test.go
+++ ShooterSub/DownloadSub_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestDefaultConfig(t *testing.T) {
+func TestDownloadSub(t *testing.T) {
 	fileName := "E:\\DP\\CentOS-7.0-1406-x86_64-Everything.iso"
 	lang := "chn"
 	downloadSub(fileName, lang)
